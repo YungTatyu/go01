1dd8090c16f84d21f0211675c8945359fcca8c9a
tatyu <tatyu@t.local>
2024-06-30 02:51:46 +0900

    :seedling:feat: ex05

---
+++ ex05/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	fmt.Println(piscine.StrLen("test"))
+	fmt.Println(piscine.StrLen("test "))
+	fmt.Println(piscine.StrLen(""))
+}
---
+++ ex05/vendor/piscine/piscine.go
@@ -0,0 +1,9 @@
+package piscine
+
+func StrLen(s string) int {
+	var i int = 0
+	for range s {
+		i++
+	}
+	return i
+}
