04210a62cbeee7e31cd5663fa0284d160925f9bf
tatyu <tatyu@t.local>
2024-06-30 16:31:00 +0900

    :seedling:feat: ex08

---
+++ ex08/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	fmt.Println(piscine.BasicAtoi("0000"))
+	fmt.Println(piscine.BasicAtoi("1234"))
+	fmt.Println(piscine.BasicAtoi("123456789"))
+}
---
+++ ex08/test/piscine_test.go
@@ -0,0 +1,22 @@
+package test
+
+import (
+	"piscine"
+	"testing"
+)
+
+func expect(actual, expect int, t *testing.T) {
+	if actual != expect {
+		t.Errorf("expected%d, but got %d", expect, actual)
+	}
+}
+
+func TestBasicAtoi(t *testing.T) {
+	expect(piscine.BasicAtoi(""), 0, t)
+	expect(piscine.BasicAtoi("test"), 0, t)
+	expect(piscine.BasicAtoi("42"), 42, t)
+	expect(piscine.BasicAtoi("000000000000000000042"), 42, t)
+	expect(piscine.BasicAtoi("0000000000000000000"), 0, t)
+	expect(piscine.BasicAtoi("9223372036854775807"), 9223372036854775807, t)
+	expect(piscine.BasicAtoi("9223372036854775808"), -9223372036854775808, t)
+}
